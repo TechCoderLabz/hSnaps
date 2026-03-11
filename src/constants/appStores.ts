53f08f4e27311618f7a8206c0460acd9a1b46910
Sagar <sag333ar@gmail.com>
2026-03-11 07:12:06 +0530

    fixed link

--- src/constants/appStores.ts
+++ src/constants/appStores.ts
@@ -3,7 +3,7 @@
  * Replace store URLs with real links when published.
  */
 export const APP_STORE_URL = 'https://apps.apple.com/app/id0000000000'
-export const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.hivesnaps'
+export const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=one.sagarkothari88.hsnaps'
 
 /** 64x64 logos in public folder. */
 export const APP_STORE_LOGO = '/appstore.png'
